c35e54021fdffa7ada6fdd4f02494275ef08395c
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-01-13 11:25:16 -0300

    add noteConatainer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar'
+import NoteContainer from '../components/NoteContainer'
 
 export default function Home() {
   return (
   <>
    <Navbar/>
+   <NoteContainer/>
   </>
   )
 }

0a83a985b84600222d2c88f6845586f4d1217ed4
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-01-27 15:07:28 -0300

    feat: New Title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,29 +2,22 @@ import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
-
 function App({ Component, pageProps }) {
   return (
     <>
       <ChakraProvider resetCSS theme={theme}>
-  
-          <Head>
-            <title>Med Triage</title>
+        <Head>
+          <title>Med Triage</title>
 
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-              rel="stylesheet"
-            />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
 
-            <meta name="description" content="A simple project" />
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-          </Head>
+          <meta name="description" content="A simple project" />
+        </Head>
 
-          <Component {...pageProps} />
- 
+        <Component {...pageProps} />
       </ChakraProvider>
     </>
   )

63f2a3d3b15a93828556d46823053f8e1adf07e6
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-01-27 22:18:41 -0300

    feat: new color for yellow.700

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,11 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+  colors: {
+    yellow: {
+      700: '#fbac0e'
+    }
+  },
   fonts: {
     heading: 'Poppins',
     body: 'Poppins'
@@ -8,7 +13,7 @@ export const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: 'gray.500',
+        bg: 'gray.700',
         color: 'gray.100'
       }
     }

ee273d969d5f761e82ca59e4d4ccf226371e6210
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-07 15:42:07 -0300

    fix: bug in lines

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,28 @@
 import Head from 'next/head'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <ChakraProvider resetCSS theme={theme}>
-        <Head>
-          <title>Med Triage</title>
+      <DndProvider backend={HTML5Backend}>
+        <ChakraProvider resetCSS theme={theme}>
+          <Head>
+            <title>Med Triage</title>
 
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+              rel="stylesheet"
+            />
 
-          <meta name="description" content="A simple project" />
-        </Head>
+            <meta name="description" content="A simple project" />
+          </Head>
 
-        <Component {...pageProps} />
-      </ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </DndProvider>
     </>
   )
 }

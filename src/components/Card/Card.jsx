0dca96e2edee1959a753b2e9f4ad4034304b2083
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-07 15:39:24 -0300

    feat: add card drag n drop

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,11 +9,69 @@ import {
   Badge
 } from '@chakra-ui/react'
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
+import { useDrag, useDrop } from 'react-dnd'
 import axios from 'axios'
 
 import { useRouter } from 'next/router'
+import { useRef } from 'react'
+// import BoardContext from '../../context/context'
 
-export default function Card(props) {
+export default function Card(props, id, index, moveCard) {
+  const ref = useRef(null)
+  // const { move } = useContext(BoardContext)
+  const [, drop] = useDrop({
+    accept: 'CARD',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId()
+      }
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+
+      if (dragIndex === hoverIndex) {
+        return
+      }
+
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+      const clientOffset = monitor.getClientOffset()
+
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+
+      moveCard(dragIndex, hoverIndex)
+
+      item.index = hoverIndex
+    }
+  })
+  const [{ isDragging }, drag] = useDrag({
+    type: 'CARD',
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+  const opacity = isDragging ? 0 : 1
+  console.log(opacity)
+  drag(drop(ref))
+
   //refresh
   const router = useRouter()
   const refreshData = () => {
@@ -52,6 +110,7 @@ export default function Card(props) {
   return (
     <Center py={6}>
       <Box
+        ref={ref}
         maxW={'320px'}
         w={'full'}
         bg="gray.900"

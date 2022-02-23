35f60490522c9ad9b891e6cca5d03983954a6a43
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-23 01:38:12 -0300

    feat: remove react-DnD

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,25 @@
 import Head from 'next/head'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd'
+
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <DndProvider backend={HTML5Backend}>
-        <ChakraProvider resetCSS theme={theme}>
-          <Head>
-            <title>Med Triage</title>
+      <ChakraProvider resetCSS theme={theme}>
+        <Head>
+          <title>Med Triage</title>
 
-            <link
-              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-              rel="stylesheet"
-            />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
 
-            <meta name="description" content="A simple project" />
-          </Head>
+          <meta name="description" content="A simple project" />
+        </Head>
 
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </DndProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   )
 }
